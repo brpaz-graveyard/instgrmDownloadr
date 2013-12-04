a45bc686ee15f43a63189164744181862213b0a7
Bruno Paz <brunopaz@sapo.pt>
2013-12-04 23:28:29 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# define our source to loook for gems
+source "http://rubygems.org/"
+ruby "1.9.3"
+
+# declare the sinatra dependency
+gem "sinatra" 
+gem "json"
+gem "instagram"
+gem "rubyzip"
---
+++ app.rb
@@ -0,0 +1,19 @@
+# If you're using Ruby 1.9 you'll need to specifically load rubygems
+require 'rubygems'
+
+# and now load bundler with your dependencies load paths
+require 'bundler/setup'
+
+# next you'll have to do the gem requiring yourself
+require 'sinatra'
+require 'instagram'
+require 'json'
+require 'securerandom'
+
+get '/' do
+ erb :index
+end
+
+get '/terms-and-conditions' do
+  'Hello world!'
+end
